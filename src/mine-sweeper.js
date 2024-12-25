e528d31cd39ffc316b9a74521fa153e136c5de2f
asfound <fffound27@gmail.com>
2024-12-25 23:06:04 +0100

    feat: solve mine-sweeper subtask

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  const board = Array.from({ length: rows }, () => Array(columns).fill(0));
+
+  for (let row = 0; row < rows; row += 1) {
+    for (let column = 0; column < columns; column += 1) {
+      if (matrix[row][column] === true) {
+        for (let rowStep = -1; rowStep <= 1; rowStep += 1) {
+          for (let columnStep = -1; columnStep <= 1; columnStep += 1) {
+            const isColumnValid =
+              column + columnStep >= 0 && column + columnStep < columns;
+            const isRowValid = row + rowStep >= 0 && row + rowStep < rows;
+
+            if (isColumnValid && isRowValid) {
+              if (rowStep !== 0 || columnStep !== 0) {
+                board[row + rowStep][column + columnStep] += 1;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return board;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
